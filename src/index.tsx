f7c7b5cf4453698241f61481930a92ebc9196540
“richmond” <richmond@osspesecuritycenter.com>
2025-01-15 12:39:51 -0800

    3 commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';  // Import the CSS file
 
 import App from './App';  // Import the App component correctly
+import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./public/index.html",
+        "./src/**/*.{html,js,jsx,ts,tsx,css}",  // Include all files inside the src folder
+        "./public/index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  "compilerOptions": {
-    "strict": true,
-    "esModuleInterop": true,
-    "jsx": "react-jsx",  // This ensures JSX works properly with React 17 and newer
-    "lib": ["dom", "es2015"],
-    "types": ["react", "react-dom"]  // Ensure these are listed here
-  },
-  "include": [
-    "src/**/*"
-  ]
 }
 
